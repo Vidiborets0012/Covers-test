3fa1db206dc34396d6511f486ad4faf14a87f1b3
Vidiborets Viktor <vidiboretsv@gmail.com>
2024-09-07 14:18:19 +0300

    add the IntersectionObserver event

--- src/main.js
+++ src/main.js
@@ -1,20 +1,15 @@
-// const coverSection = document.querySelector('.covers');
-// const coverList = document.querySelector('.covers-list');
+const coversSection = document.querySelector('.covers');
 
-// console.log('coverSection:', coverSection);
-// console.log('coverList:', coverList);
+const observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      // Додаємо клас для запуску анімації
+      coversSection.classList.add('animate');
+    } else {
+      // Видаляємо клас, якщо секція вийшла з видимості
+      coversSection.classList.remove('animate');
+    }
+  });
+});
 
-// const observer = new IntersectionObserver((entries) => {
-//     entries.forEach(entry => {
-//         if (entry.isIntersecting) {
-//             coverList.classList.add('covers-list-animated');
-//         } else {
-//             coverList.classList.remove('covers-list-animated');
-//         }
-//     });
-
-// }, {
-//     treshold: 0.1
-// });
-
-// observer.observe(coverSection);
+observer.observe(coversSection);
