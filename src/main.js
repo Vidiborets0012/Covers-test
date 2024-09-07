ba07832ec49d226847ef3f2519c319f905808e2a
Vidiborets Viktor <vidiboretsv@gmail.com>
2024-09-07 13:47:49 +0300

    add the basis of the covers section

---
+++ src/main.js
@@ -0,0 +1,20 @@
+// const coverSection = document.querySelector('.covers');
+// const coverList = document.querySelector('.covers-list');
+
+// console.log('coverSection:', coverSection);
+// console.log('coverList:', coverList);
+
+// const observer = new IntersectionObserver((entries) => {
+//     entries.forEach(entry => {
+//         if (entry.isIntersecting) {
+//             coverList.classList.add('covers-list-animated');
+//         } else {
+//             coverList.classList.remove('covers-list-animated');
+//         }
+//     });
+
+// }, {
+//     treshold: 0.1
+// });
+
+// observer.observe(coverSection);
