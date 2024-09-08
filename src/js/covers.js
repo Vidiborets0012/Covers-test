910688c8959536e6ada9d79ad1f90c8a2de9530c
Vidiborets Viktor <vidiboretsv@gmail.com>
2024-09-08 15:43:27 +0300

    make changes to js and styles

---
+++ src/js/covers.js
@@ -0,0 +1,15 @@
+export function initCoversAnimation() {
+  const coversSection = document.querySelector('.covers');
+
+  const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        coversSection.classList.add('animate'); // Додає клас для запуску анімації
+      } else {
+        coversSection.classList.remove('animate'); // Видаляє клас, якщо секція виходить з видимості
+      }
+    });
+  });
+
+  observer.observe(coversSection);
+}
--- src/main.js
+++ src/main.js
@@ -1,15 +1,36 @@
-const coversSection = document.querySelector('.covers');
+// const coversSection = document.querySelector('.covers');
 
-const observer = new IntersectionObserver(entries => {
-  entries.forEach(entry => {
-    if (entry.isIntersecting) {
-      // Додаємо клас для запуску анімації
-      coversSection.classList.add('animate');
-    } else {
-      // Видаляємо клас, якщо секція вийшла з видимості
-      coversSection.classList.remove('animate');
-    }
-  });
-});
+// const observer = new IntersectionObserver(entries => {
+//   entries.forEach(entry => {
+//     if (entry.isIntersecting) {
+//       // Додаємо клас для запуску анімації
+//       coversSection.classList.add('animate');
+//     } else {
+//       // Видаляємо клас, якщо секція вийшла з видимості
+//       coversSection.classList.remove('animate');
+//     }
+//   });
+// });
 
-observer.observe(coversSection);
+// const coversSection = document.querySelector('.covers');
+// const coversList = document.querySelector('.covers-list');
+
+// const observer = new IntersectionObserver(entries => {
+//   entries.forEach(entry => {
+//     if (entry.isIntersecting) {
+//       // Запускаємо анімацію при появі секції
+//       coversList.style.animationPlayState = 'running';
+//     } else {
+//       // Зупиняємо анімацію, якщо секція вийшла з видимості
+//       coversList.style.animationPlayState = 'paused';
+//     }
+//   });
+// });
+
+// observer.observe(coversSection);
+
+import { initCoversAnimation } from "./js/covers";
+
+document.addEventListener('DOMContentLoaded', () => {
+  initCoversAnimation();
+})
